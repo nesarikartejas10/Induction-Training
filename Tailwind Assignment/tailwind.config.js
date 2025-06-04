b98f2c6f1392ff00fe81a60397712c5eae07e98a
nesarikartejas10 <nesarikartejas10@gmail.com>
2025-06-04 10:34:27 +0530

    Tailwind Assignment

---
+++ Tailwind Assignment/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+      colors: {
+        primary: "#1abc9c",
+        secondary: "#2c3e50",
+        dark: "#1a252f",
+      },
+    },
+  },
+  plugins: [],
+};
